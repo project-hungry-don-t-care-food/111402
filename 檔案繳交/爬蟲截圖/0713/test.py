4ae8d2aa648a5f35bb84b00fa79a50fb50149112
N1086426 <97925675+N1086426@users.noreply.github.com>
2022-09-25 21:17:46 +0800

    Add files via upload

---
+++ 檔案繳交/爬蟲截圖/0713/test.py
@@ -0,0 +1,13 @@
+import csv
+# 開啟 CSV 檔案
+with open('Restaurant data.csv', 'r',encoding="utf-8") as csvfile:
+    # 讀取 CSV 檔案內容
+    reader = csv.DictReader(csvfile)
+    column = [row['餐廳連結'] for row in reader]
+    print(column[2])
+
+for index, row in enumerate(column):
+    print("目前：" + str(index))
+    url=(row[0:])
+    print(url)
+  
