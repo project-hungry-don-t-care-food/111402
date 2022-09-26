422ee9778b23284f5424d205577de963eee88e8d
10846043 <10846043@ntub.edu.tw>
2022-09-26 21:06:26 +0800

    update APP_10846043

--- 檔案繳交/APP_10846043/ConnectionToSql_JAVA/app/src/main/java/com/example/connectiontosql_java/MainActivity.java
+++ 檔案繳交/APP_10846043/ConnectionToSql_JAVA/app/src/main/java/com/example/connectiontosql_java/MainActivity.java
@@ -2,10 +2,16 @@ package com.example.connectiontosql_java;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.sql.Blob;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
@@ -15,14 +21,17 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     TextView textview;
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Connection connection;
         textview = findViewById(R.id.textView);
+        imageView = findViewById(R.id.imageView);
+
         List<String> names = new ArrayList<String>();
+        Connection connection;
         try {
             ConSQL c = new ConSQL();
             connection = c.conclass();
@@ -30,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
             Statement smt = connection.createStatement();
             ResultSet set = smt.executeQuery(sqlstatement);
             while (set.next()){
-                names.add(set.getString(1));
+                //names.add(set.getString(1));
             }
             textview.setText(names.toString());
 
@@ -39,6 +48,29 @@ public class MainActivity extends AppCompatActivity {
             Log.d("SqlCon1",e.toString());
         }
 
+        try {
+            ConSQL c = new ConSQL();
+            connection = c.conclass();
+            String sqlstatement = "SELECT image FROM store_image where store_info_id = 155";
+            Statement smt = connection.createStatement();
+            ResultSet set = smt.executeQuery(sqlstatement);
+            while (set.next()){
+                //names.add(set.getString(1));
+                String image = set.getString(1);
+
+                byte[] bytes = Base64.decode(image,Base64.NO_CLOSE);
+                Bitmap bitmap = BitmapFactory.decodeByteArray(bytes,0,bytes.length);
+
+
+                imageView.setImageBitmap(bitmap);
+                Toast.makeText(this,"Finish",Toast.LENGTH_LONG).show();
+            }
+            textview.setText(names.toString());
+
+            connection.close();
+        }catch (Exception e){
+            Log.d("SqlCon1",e.toString());
+        }
 
     }
 }
